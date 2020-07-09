f43a0e6d12323dbfc9fc3586213660889657459d
charles-r-earp <charles.r.earp@gmail.com>
2020-07-08 19:12:50 -0700

    oneDNN only supports opencl gpus, disabled cpu opencl tests.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,6 @@
 use onednn_sys::*;
 #[cfg(feature="ocl")]
-use ocl::Context;
+use ocl::{Platform, Device, Context, enums::{DeviceInfo, DeviceInfoResult}, flags::DeviceType};
 
 #[test]
 fn test_engine_create_cpu() {
@@ -14,22 +14,48 @@ fn test_engine_create_cpu() {
 }
 
 #[cfg(feature="ocl")]
+fn get_gpu_context() -> Option<Context> {
+    for platform in Platform::list() {
+        if let Ok(devices) = Device::list(&platform, Some(DeviceType::new().gpu())) {
+            for device in devices {
+                if let Ok(context) = Context::builder()
+                    .platform(platform)
+                    .devices(device)
+                    .build() {
+                    return Some(context);
+                } 
+            }
+        }
+    }
+    None
+} 
+
+#[cfg(feature="ocl")]
 #[test]
 fn test_engine_create_ocl() {
-    let context = Context::builder()
-        .build()
-        .unwrap();
-    let device = context.devices()[0];
-    let mut engine = std::ptr::null_mut();
-    let status = unsafe { 
-        dnnl_engine_create_ocl(
-            &mut engine as *mut *mut dnnl_engine, 
-            dnnl_engine_kind_t::dnnl_gpu, 
-            device.as_raw() as onednn_sys::cl_device_id, 
-            context.as_ptr() as onednn_sys::cl_context
-        )
-    };
-    assert_eq!(status, dnnl_status_t::dnnl_success);
-    let status = unsafe { dnnl_engine_destroy(engine) };
-    assert_eq!(status, dnnl_status_t::dnnl_success);
+    if let Some(context) = get_gpu_context() {
+        let device = context.devices()[0];
+        let device_type = match device.info(DeviceInfo::Type) {
+            Ok(DeviceInfoResult::Type(device_type)) => device_type,
+            _ => panic!()
+        };
+        let engine_kind = if device_type == DeviceType::new().gpu() {
+            dnnl_engine_kind_t::dnnl_gpu
+        }
+        else {
+            unimplemented!()
+        };
+        let mut engine = std::ptr::null_mut();
+        let status = unsafe { 
+            dnnl_engine_create_ocl(
+                &mut engine as *mut *mut dnnl_engine, 
+                engine_kind,
+                device.as_raw() as onednn_sys::cl_device_id, 
+                context.as_ptr() as onednn_sys::cl_context
+            )
+        };
+        assert_eq!(status, dnnl_status_t::dnnl_success);
+        let status = unsafe { dnnl_engine_destroy(engine) };
+        assert_eq!(status, dnnl_status_t::dnnl_success);
+    }
 }
